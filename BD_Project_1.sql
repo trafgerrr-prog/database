fb83a4e50cbe0e048f89d7fd20d3e92c5577a45b
trafgerrr <zhenyat30@gmail.com>
2021-05-27 22:58:56 +0300

    Заполнил таблицы

--- BD_Project_1.sql
+++ BD_Project_1.sql
@@ -30,3 +30,60 @@ CREATE TABLE orders(
 	FOREIGN KEY (order_id) REFERENCES order_list (id),
 	FOREIGN KEY (menu_id) REFERENCES menu (id)
 )
+
+------
+INSERT INTO menu (id, name, sostav, weight,
+				 belki, zhiri, uglevodi, kcal)
+VALUES (1,'Стейк из черпорога', '2 стейка, олив. масло, слив. масло, соль, 
+		северные пряности, чеснок измел., чеснок целый, сухое вино.', 1000, 270, 150, 0, 2520);
+
+INSERT INTO menu (id, name, sostav, weight,
+				 belki, zhiri, uglevodi, kcal)
+VALUES (2,'Жаренные ножки лесного долгонога', '4 индюшиных ножки, соль, чесночный порошок,
+		молотый кориандр, сушеный майоран, паприка, черный перец.', 2000, 390, 134, 0, 2880);
+		
+INSERT INTO menu (id, name, sostav, weight,
+				 belki, zhiri, uglevodi, kcal)
+VALUES (3,'Яйца, запеченные с травами', '4 кур. яйца, тимьян, розмарин, петрушка, соль, черный перец,
+        тертый пармезан, сливки, слив. масло, хлеб.', 120, 15, 11, 1, 171);
+		
+INSERT INTO menu (id, name, sostav, weight,
+				 belki, zhiri, uglevodi, kcal)
+VALUES (4,'Чудесный вишневый пирог', 'Вишня, мука, слив. масло, вишневый сок, сахар, 
+		кукурузный крахмал, корица, молотый кардамон, молотый имбирь, соль, кур. яйцо', 800, 40, 13, 208, 1000);
+------
+INSERT INTO users (id, name, address, phone_number)
+VALUES (1, 'Петр', 'г. Дубна, ул. Ленинградская, д. 14', '88005553535');
+
+INSERT INTO users (id, name, address, phone_number)
+VALUES (2, 'Иван', 'г. Москва, ул. Пушкина, д. Колотушкина', '89001234567');
+------
+INSERT INTO order_list (id, user_id, order_status)
+VALUES (100, 1, 'Доставлено')
+
+INSERT INTO order_list (id, user_id, order_status)
+VALUES (101, 1, 'Готовится')
+
+INSERT INTO order_list (id, user_id, order_status)
+VALUES (102, 2, 'Готовится')
+
+INSERT INTO order_list (id, user_id, order_status)
+VALUES (103, 2, 'Доставляется')
+-----
+INSERT INTO orders (order_id, menu_id, date_order)
+VALUES (100, 1, now())
+
+INSERT INTO orders (order_id, menu_id, date_order)
+VALUES (100, 3, now())
+
+INSERT INTO orders (order_id, menu_id, date_order)
+VALUES (101, 1, now())
+
+INSERT INTO orders (order_id, menu_id, date_order)
+VALUES (102, 2, now())
+
+INSERT INTO orders (order_id, menu_id, date_order)
+VALUES (102, 4, now())
+
+INSERT INTO orders (order_id, menu_id, date_order)
+VALUES (103, 4, now())
